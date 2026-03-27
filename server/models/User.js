eafb8b4cfd7730f67942813247481c3d07139533
with-Khilraj <khilstha75@gmail.com>
2026-03-27 10:22:28 +0545

    feat (models): Create User schema

---
+++ server/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true},
+    role: { type: String, enum: ['buyer', 'seller', 'admin'], default: 'buyer'},
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
